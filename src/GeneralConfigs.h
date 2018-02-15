beeca13881310300df1033f0016cae3722b50e25
Martin Teuber <martinteuber@web.de>
2018-02-15 13:12:02 +0100

    make it compileable for presentation

--- src/GeneralConfigs.h
+++ src/GeneralConfigs.h
@@ -8,6 +8,9 @@
 #define REDIS_HOST ("127.0.0.1")
 #define REDIS_PORT (7000)
 
+#define CASSANDRA_HOST ("localhost")
+#define CASSANDRA_PORT ( 32774 )
+
 enum Operation {
     SUCCESS_ = 0,
     ERROR_ = 1,
--- src/cassandra_handler.h
+++ src/cassandra_handler.h
@@ -2,12 +2,17 @@
 #define CASSANDRA_HANDLER_H
 
 #include <string>
+#include <vector>
+#include <cassandra.h>
 
 class Cassandra_Handler
 {
 public:
     ~Cassandra_Handler();
     static Cassandra_Handler *instance();
+
+    void initCassandra();
+    void terminateCassandra();
 
     bool saveSoundComponent(std::vector<std::pair<std::string, std::string>>* sounds );
     bool loadSoundComponent(std::string& pName,std::vector<std::vector<std::pair<std::string, std::string>>*>& sounds);
@@ -21,6 +26,10 @@ private:
     }
 
     static Cassandra_Handler *cassandra;
+
+    static CassCluster *cluster;
+    static CassSession *session;
+    static CassFuture *connect_future;
 
 };
 #endif // CASSANDRA_HANDLER_H
